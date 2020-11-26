5a814d196e57dd2c87b5a50e1a58756ee08f47ab
EugeneRikka <73299664+EugeneRikka@users.noreply.github.com>
2020-11-26 06:15:58 +0300

    Анимированный фон

---
+++ TWins game/Assets/Scenes/Scripts/BackgroundLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    public float speed = 0.0005f;
+    private Vector2 offset = Vector2.zero;
+    private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = material.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x += speed; // * Time.deltaTime;
+        material.SetTextureOffset("_MainTex", offset);
+    }
+}
